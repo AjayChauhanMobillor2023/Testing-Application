0b1cfaec231186b6da753f4098aff0f34dec64ce
Ajay Chauhan <ajay.chauhan@mobillor.com>
2024-09-19 14:23:51 +0530

    first commit

--- ToasterToast/build.gradle.kts
+++ ToasterToast/build.gradle.kts
@@ -32,6 +32,9 @@ android {
         kotlinOptions {
                 jvmTarget = "1.8"
         }
+        group = "com.github.AjayChauhanMobillor2023"  // Use your GitHub username
+        version = "'1.0.0' "                  // Version of your library
+
 }
 
 dependencies {
